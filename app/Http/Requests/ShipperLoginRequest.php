ecb1e8650ba6e10996e261420e4af83ac7c8df05
Quốc Sang <sang0882k3@gmail.com>
2025-02-19 14:00:09 +0700

    fix

--- app/Http/Requests/ShipperLoginRequest.php
+++ app/Http/Requests/ShipperLoginRequest.php
@@ -23,12 +23,12 @@ class ShipperLoginRequest extends FormRequest
     public function messages()
     {
         return [
-            'email.required' => 'Email không được để trống.',
-            'email.email' => 'Email không đúng định dạng.',
-            'email.exists' => 'Email không tồn tại.',
+            'email.required'    => 'Email không được để trống.',
+            'email.email'       => 'Email không đúng định dạng.',
+            'email.exists'      => 'Email không tồn tại.',
             'password.required' => 'Mật khẩu không được để trống.',
-            'password.min' => 'Mật khẩu phải có ít nhất 6 ký tự.',
-            'password.max' => 'Mật khẩu không được quá 255 ký tự.',
+            'password.min'      => 'Mật khẩu phải có ít nhất 6 ký tự.',
+            'password.max'      => 'Mật khẩu không được quá 255 ký tự.',
         ];
     }
 }

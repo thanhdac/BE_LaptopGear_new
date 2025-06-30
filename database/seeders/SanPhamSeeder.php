2380c55754bb1ce9bb62ba18d6fc0aa0a773fff2
NguyenTrongTinh190204 <NguyenTrongTinh190204@gmail.com>
2025-06-30 16:49:34 +0700

    hello

--- database/seeders/SanPhamSeeder.php
+++ database/seeders/SanPhamSeeder.php
@@ -15,6 +15,7 @@ class SanPhamSeeder extends Seeder
         DB::table('san_phams')->truncate();
         DB::table('san_phams')->insert([
             [
+
                 'ma_sp'         => 1,
                 'ten_sp'        => 'Laptop Dell XPS 13',
                 'don_gia'       => 30000000,
@@ -23,7 +24,7 @@ class SanPhamSeeder extends Seeder
                 'hinh'          => 'https://hanoilab.com/wp-content/uploads/2022/08/Dell-xps-13-plus-9320-hanoilab.jpg',
                 'ma_dm'         => 1,
                 'mo_ta'         => 'Laptop Dell XPS 13 với thiết kế mỏng nhẹ, hiệu năng mạnh mẽ, phù hợp cho công việc và giải trí.',
-                'trailer'       => 'https://www.youtube.com/watch?v=example1'
+                'trailer'       => 'https://www.youtube.com/watch?v=fmSmcSfC5fk'
             ],
             [
                 'ma_sp'         => 2,
@@ -34,7 +35,7 @@ class SanPhamSeeder extends Seeder
                 'hinh'          => 'https://hanoilab.com/wp-content/uploads/2022/08/Dell-xps-13-plus-9320-hanoilab.jpg',
                 'ma_dm'         => 2,
                 'mo_ta'         => 'Laptop HP Pavilion, hiệu năng ổn định, thiết kế sang trọng, phù hợp với nhu cầu học tập và làm việc.',
-                'trailer'       => 'https://www.youtube.com/watch?v=example2'
+                'trailer'       => 'https://www.youtube.com/watch?v=fmSmcSfC5fk'
             ],
             [
                 'ma_sp'         => 3,
@@ -45,7 +46,7 @@ class SanPhamSeeder extends Seeder
                 'hinh'          => 'https://hanoilab.com/wp-content/uploads/2022/08/Dell-xps-13-plus-9320-hanoilab.jpg',
                 'ma_dm'         => 3,
                 'mo_ta'         => 'Macbook Pro 16 với hiệu năng vượt trội, thiết kế tinh tế, phù hợp cho các chuyên gia sáng tạo.',
-                'trailer'       => 'https://www.youtube.com/watch?v=example2'
+                'trailer'       => 'https://www.youtube.com/watch?v=fmSmcSfC5fk'
             ]
         ]);
 
